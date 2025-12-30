ae52106fda7c0373c7fa36b0231a1b84171ec251
Lucas Bouchard <lucas.bouchard@polymtl.ca>
2025-12-30 17:28:49 -0500

    Exercises 2.18 through 2.20 (previous commit in error)

---
+++ part2/dataForCountries/src/services/weather.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY
+
+const getWeather = (lat, lon) => {
+    const request = axios.get(`${baseUrl}lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    return request.then(response => response.data)
+}
+
+export default {
+    getWeather
+}
