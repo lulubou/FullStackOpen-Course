9de30646a9bea135b6a55e06083aafe834e0aeb1
Lucas Bouchard <lucas.bouchard@polymtl.ca>
2026-01-02 19:09:34 -0500

    Exercises 3.7 and 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => {
     const minId = 5
