359259e566636b753c73baca8ec6b6fda8e23935
Lucas Bouchard <lucas.bouchard@polymtl.ca>
2026-01-02 22:15:56 -0500

    Small changes to deploy to internet

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
@@ -17,22 +19,22 @@ let persons = [
     {
         "id": "1",
         "name": "Arto Hellas",
-        "number": "040-123456"
+        "phoneNumber": "040-123456"
     },
     {
         "id": "2",
         "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        "phoneNumber": "39-44-5323523"
     },
     {
         "id": "3",
         "name": "Dan Abramov",
-        "number": "12-43-234345"
+        "phoneNumber": "12-43-234345"
     },
     {
         "id": "4",
         "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        "phoneNumber": "39-23-6423122"
     }
 ]
 
@@ -67,9 +69,9 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
+    if (!body.name || !body.phoneNumber) {
         return response.status(400).json({
-            error: 'Name or number is missing.'
+            error: 'Name or phone number is missing.'
         })
     }
 
@@ -82,7 +84,7 @@ app.post('/api/persons', (request, response) => {
     const person = {
         id: generateId(),
         name: body.name,
-        number: body.number
+        phoneNumber: body.phoneNumber
     }
 
     persons = persons.concat(person)
@@ -90,7 +92,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
